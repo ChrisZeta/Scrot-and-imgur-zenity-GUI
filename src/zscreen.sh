5dfee68919ca40e4fa08375ef2c0704d2d9a823c
ChrisZeta <thewebcha@gmail.com>
2012-09-02 18:57:43 +0200

    primo commit

---
+++ src/zscreen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# zscreen script by Christian Zucchelli (@Chris_Zeta) <thewebcha@gmail.com>
+
+ans=$(zenity  --list  --text "Is linux.byexamples.com helpful?" --radiolist  --column "Pick" --column "Options" TRUE "Selected Area... (click & drag mouse)" FALSE "Now" FALSE "With delay"); 
+
+case $ans in
+
+"Selected Area... (click & drag mouse)" ) 
+
+zenity --question --text "Do you want upload the screenshot?"
+if [ "$?" = "0" ]; then 
+scrot -s '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/ & zimgur ~/Immagini/Screenshot/$f'
+else
+scrot -s '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/'
+fi;;
+
+"Now" ) zenity --question --text "Do you want upload the screenshot?"
+if [ "$?" = "0" ]; then 
+scrot '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/ & zimgur ~/Immagini/Screenshot/$f'
+else
+scrot '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/'
+fi;;
+
+"In n second" ) if zenity --entry --title="Second delay" --text="Enter second of delay:" --entry-text "5"
+then echo $?
+
+zenity --question --text "Do you want upload the screenshot?"
+if [ "$?" = "0" ]; then 
+scrot -d $? '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/ & zimgur ~/Immagini/Screenshot/$f'
+else
+scrot -d $? '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'mv $f ~/Immagini/Screenshot/'
+fi
+fi;;
+
+esac
