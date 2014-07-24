31cff972085f93bb9bff3c99b1d457d2bf6c8c8c
Erik Westrup <erik.westrup@gmail.com>
2014-07-24 20:15:08 +0200

    Refactor code and add simple config file support.

--- src/zscreen.sh
+++ src/zscreen.sh
@@ -1,43 +1,59 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # zscreen script by Christian Zucchelli (@Chris_Zeta) <thewebcha@gmail.com>
 
-if [ ! -d "$HOME/.zscreen" ]
-then
-mkdir ~/.zscreen
-fi
+read_conf() {
+	if [ ! -d "$HOME/.zscreen" ]; then
+		mkdir $HOME/.zscreen
+	fi
 
-cd ~
-if [ ! -d "Screenshots" ]
-then mkdir ~/Screenshots
-fi
+	if [ -e "$HOME/.zscreen/conf.sh" ]; then
+		. $HOME/.zscreen/conf.sh
+	fi
 
-ans=$(zenity --width 350 --height 220 --list --text "Screenshot mode" --radiolist --column "Pick" --column "Options" TRUE "Selected Area... (click & drag mouse)" FALSE "Now" FALSE "With delay");
+	# Set default values.
+	if [ -z "$ZSCREEN_SCREENSHOT_DIR" ]; then
+		export ZSCREEN_SCREENSHOT_DIR="$HOME/Screenshots"
+	fi
+	if [ -z "$ZSCREEN_FILEFMT" ]; then
+		export ZSCREEN_FILEFMT=='%Y-%m-%d--%s_$wx$h_scrot.png'
+	fi
 
-case $ans in
+	if [ ! -d "$ZSCREEN_SCREENSHOT_DIR" ]; then
+		mkdir -p "$ZSCREEN_SCREENSHOT_DIR"
+	fi
+}
 
-"Selected Area... (click & drag mouse)" )
+use_scrot() {
+	local upload="$1"
+	local scrotargs="$2"
 
+	local cmdline="scrot ${scrotargs} \${ZSCREEN_FILEFMT} -e \"sleep 2 & mv \\"'$'"f ${ZSCREEN_SCREENSHOT_DIR}"
+	if [ "$upload" == "0" ]; then
+		cmdline="$cmdline & zimgur ${ZSCREEN_SCREENSHOT_DIR}/\\"'$'"f"
+	fi
+	cmdline="$cmdline\""
+	eval "$cmdline"
+}
+
+read_conf
+
+opt_clickdrag="Selected Area... (click & drag mouse)"
+opt_now="Now"
+opt_delay="With delay"
+
+mode=$(zenity --width 350 --height 220 --list --text "Screenshot mode" --radiolist --column "Pick" --column "Options" TRUE "$opt_clickdrag" FALSE "$opt_now" FALSE "$opt_delay");
 zenity --question --text "Do you want upload the screenshot?"
-if [ "$?" = "0" ]; then
-scrot -s '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/ & zimgur ~/Screenshots/$f'
-else
-scrot -s '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/'
-fi;;
+upload="$?"
 
-"Now" ) zenity --question --text "Do you want upload the screenshot?"
-if [ "$?" = "0" ]; then
-scrot -d 1 '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/ & zimgur ~/Screenshots/$f'
-else
-scrot -d 1 '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/'
-fi;;
-
-"With delay" ) d=$(zenity --entry --title="With delay" --text="Enter seconds of delay:" --entry-text "5")
-zenity --question --text "Do you want upload the screenshot?"
-if [ "$?" = "0" ]; then
-scrot -d "$d" '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/ & zimgur ~/Screenshots/$f'
-else
-scrot -d "$d" '%Y-%m-%d--%s_$wx$h_scrot.png' -e 'sleep 2 & mv $f ~/Screenshots/'
-fi;;
-
+case "$mode" in
+	"$opt_clickdrag")
+		use_scrot "$upload" "-s"
+		;;
+	"$opt_now")
+		use_scrot "$upload" "-d 1"
+		;;
+	"$opt_delay")
+		delay=$(zenity --entry --title="With delay" --text="Enter seconds of delay:" --entry-text "5")
+		use_scrot "$upload" "-d $delay"
+		;;
 esac
